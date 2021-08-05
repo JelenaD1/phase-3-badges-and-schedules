f0d6feb5a313628d45da58c145b0180599cbef1c
Jelena <jelena.durovic@powerhrg.com>
2021-08-05 13:45:23 -0400

    commit tree 3681fa713e253e0aaf237a6ea4b054027890d8ab

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(names)
+    names.map do |name|
+        badge_maker(name)
+    end  
+end 
+
+def assign_rooms(speakers)
+    speakers.map.each_with_index do |speaker, index|
+        "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+    end 
+end 
+
+def printer(attendees)
+   batch_badge_creator(attendees).map do |attendee|
+    puts attendee
+   end 
+     assign_rooms(attendees).map do |attendee|
+        puts attendee
+   end 
+end 
